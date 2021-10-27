01c5db2f8779cb260c8e5adf4b36bf07a41bd5a4
Xel4ek <l-baal@yandex.ru>
2021-10-27 12:28:49 +0300

    virtual host devices

--- apps/light/src/app/components/share/device-list/device-list.module.ts
+++ apps/light/src/app/components/share/device-list/device-list.module.ts
@@ -4,6 +4,8 @@ import { DeviceListComponent } from './device-list/device-list.component';
 import { LoadScreenModule } from '@modules/load-screen/load-screen.module';
 import { MobileDeviceWidgetModule } from '@components/mobile/mobile-device-widget/mobile-device-widget.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TuiScrollbarModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [DeviceListComponent],
@@ -12,6 +14,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     LoadScreenModule,
     MobileDeviceWidgetModule,
     DragDropModule,
+    ScrollingModule,
+    TuiScrollbarModule,
   ],
   exports: [DeviceListComponent],
 })
