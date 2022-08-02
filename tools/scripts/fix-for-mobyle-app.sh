77d434838fe32c001b70e1f2248831cdd09ac0e3
Poltorak Serguei <ps@z-wave.me>
2022-08-02 03:29:40 +0300

    Removed Alexei specific paths

--- tools/scripts/fix-for-mobyle-app.sh
+++ tools/scripts/fix-for-mobyle-app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./tools/scripts/venv/Scripts/python.exe ./tools/scripts/fix-for-mobyle-app.py
+python ./tools/scripts/fix-for-mobyle-app.py
 
 DIST="./dist/apps/lite/"
 FILE=$(find $DIST -name "main.*.js")
